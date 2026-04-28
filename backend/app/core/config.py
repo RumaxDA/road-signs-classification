3412c9d6719be612cc7f17366208c113f9b4f581
Dawid Rumiński <ruminski.dawid77@gmail.com>
2026-04-28 12:40:07 +0200

    feat: configure connection with DB through .env file

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,16 @@
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv(".env"))
 
 class Settings(BaseSettings):
     # Dane bazy
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
-    POSTGRES_HOST: str
-    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str = os.environ["POSTGRES_USER"]
+    POSTGRES_PASSWORD: str = os.environ["POSTGRES_PASSWORD"]
+    POSTGRES_DB: str = os.environ["POSTGRES_DB"]
+    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "db")
+    POSTGRES_PORT: int = os.getenv("POSTGRES_PORT", 5432)
 
     YOLO_MODEL_PATH: str = "models_ai/best.pt"
     CNN_MODEL_PATH: str = "models_ai/cnn_48_v1.keras"
@@ -18,9 +22,13 @@ class Settings(BaseSettings):
 
     # Konfiguracja ładowania
     model_config = SettingsConfigDict(
-        env_file=".env", 
+        env_file= ".env",
         extra="ignore",
-        env_file_encoding="utf-8"
+        env_file_encoding="utf-8",
+        case_sensitive= True
     )
 
 settings = Settings()
+
+if __name__ == "__main__":
+    pass
